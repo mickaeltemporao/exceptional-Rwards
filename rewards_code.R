1ccbea80b25642c11acfa31292bd4a9fa6087cbd
SkelAlex <alexandre.fortier.chouinard@mail.utoronto.ca>
2024-09-29 18:58:09 -0400

    Adjustments to point scheme and message scheme

--- rewards_code.R
+++ rewards_code.R
@@ -14,16 +14,18 @@ error_handler <- function(e) {
   # Error message
   error_message <- conditionMessage(e)
   
-  # If it's a new type of error, add 3 points
-  if (!(error_message %in% error_tracker$error_types)) {
-    error_tracker$error_types[[error_message]] <- TRUE
-    error_tracker$points <- error_tracker$points + 3
+  # Check if it's a new type of error (without modifying the original error message)
+  if (!(error_message %in% names(error_tracker$error_types))) {
+    error_tracker$error_types[[error_message]] <- TRUE  # Track new error type
+    error_tracker$points <- error_tracker$points + 3     # Add 3 points for a new error type
+  } else {
+    error_tracker$points <- error_tracker$points + 1     # Add 1 point for a recurring error
   }
 
   # Assign the color based on points
   color_func <- if (error_tracker$points >= 500) {
     red
-  } else if (error_tracker$points >= 300) {
+  } else if (error_tracker$points >= 250) {
     magenta
   } else if (error_tracker$points >= 100) {
     blue
@@ -36,15 +38,22 @@ error_handler <- function(e) {
   }
 
   # Create a positive message
-  positive_messages <- c(
-    "Great learning opportunity!",
-    "You're doing awesome!",
-    "Keep going, every mistake is progress!",
-    "Nice work! You'll get this!",
+  message <- if (error_tracker$points >= 500) {
+    "You're doing a fantastic job! Way to go!"
+  } else if (error_tracker$points >= 250) {
     "You're getting better with each step!"
-  )
-  
-  message <- sample(positive_messages, 1)
+  } else if (error_tracker$points >= 100) {
+    "Nice work! You'll get this!"
+  } else if (error_tracker$points >= 50) {
+    "Keep going, every mistake is progress!"
+  } else if (error_tracker$points >= 20) {
+    "You're doing awesome!"
+  } else if (error_tracker$points >= 4) {
+    "More mistakes will keep earning you more points!"
+  } else {
+    paste("This is your first mistake. Humans learn by making mistakes and",
+          "fixing them. Keep up the good work and you will accumulate points!")
+  }
   
   # Print the message in the selected color
   message(color_func(paste("Error detected:", error_message)))
@@ -63,7 +72,7 @@ evaluate_code <- function(expr) {
   )
 }
 
-# Example of usage:
-# You can wrap your code within evaluate_code like this:
-# evaluate_code(quote(log("a")))  # This will trigger an error
 evaluate_code(quote(log("a")))
+evaluate_code("lol"+TRUE) # test with another type of error
+
+## Next step: remove the need to use evaluate_code
